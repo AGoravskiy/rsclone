186284245056f4232ac0caaffbf38d2bc702fff0
Ivan Shvets <shvets.ivan@gmail.com>
2021-01-22 22:32:02 +0300

    feat: proceed to signup

--- client/assets/sripts/intro.js
+++ client/assets/sripts/intro.js
@@ -1,9 +1,18 @@
-const video = document.querySelector("video");
+const video = document.querySelector('video');
 video.playbackRate = 4;
 
-video.addEventListener("ended", drawTitle);
+video.addEventListener('ended', drawTitle);
 
 function drawTitle() {
-  document.querySelector(".title-wrapper").classList.toggle("hide");
-  document.querySelector(".footer-wrapper").classList.toggle("hide");
+  document.querySelector('.title-wrapper').classList.toggle('hide');
+  document.querySelector('.footer-wrapper').classList.toggle('hide');
+}
+
+// const button = document.querySelector('.preload-btn');
+// if ()
+window.addEventListener('click', proceedToLogin);
+window.addEventListener('keydown', proceedToLogin);
+
+function proceedToLogin() {
+  window.location.href = 'login.html';
 }
