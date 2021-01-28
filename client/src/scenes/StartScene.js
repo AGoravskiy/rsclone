4ec039d0270280c3eaf766bf1004d2c475069762
Ivan Shvets <shvets.ivan@gmail.com>
2021-01-28 16:44:32 +0300

    feat: main menu

--- client/src/scenes/StartScene.js
+++ client/src/scenes/StartScene.js
@@ -19,16 +19,6 @@ export default class StartScene extends Phaser.Scene {
     this.createBackground();
     this.createButtons();
     this.setEvents();
-    // this.style = {
-    //   'background-color': 'lime',
-    //   'width': '220px',
-    //   'height': '100px',
-    //   'font': '48px Arial',
-    //   'font-weight': 'bold',
-    //   'cursor':"pointer",
-    //   'z-index': '10000',
-    // };
-    // this.element = this.add.dom(400, 300, 'div', this.style, 'this');
   }
 
   createSounds() {
@@ -60,100 +50,40 @@ export default class StartScene extends Phaser.Scene {
       cursor: 'pointer',
     };
 
-    const resumeBtn = this.add.text(96, 224, 'Resume', menuItemsStyle);
-    resumeBtn.setShadow(0, 4, '#0B0500', 4);
-    resumeBtn.setInteractive();
+    // const resumeBtn = this.add.text(96, 224, 'Resume', menuItemsStyle);
+    // this.resumeBtn = resumeBtn;
+    // resumeBtn.setShadow(0, 4, '#0B0500', 4);
+    // resumeBtn.setInteractive();
+    // resumeBtn.inputEnabled;
+    // resumeBtn.useHandCursor = true;
 
     const onePlayerBtn = this.add.text(96, 284, 'One player', menuItemsStyle);
+    this.onePlayerBtn = onePlayerBtn;
     onePlayerBtn.setShadow(0, 4, '#0B0500', 4);
-    // this.onePlayerBtn.setInteractive();
+    onePlayerBtn.setInteractive();
 
     const twoPlayerBtn = this.add.text(96, 344, 'Two players', menuItemsStyle);
+    this.twoPlayerBtn = twoPlayerBtn;
     twoPlayerBtn.setShadow(0, 4, '#0B0500', 4);
     twoPlayerBtn.setInteractive();
 
-    const settingsBtn = this.add.text(96, 404, 'Setting', menuItemsStyle);
+    const settingsBtn = this.add.text(96, 404, 'Settings', menuItemsStyle);
+    this.settingsBtn = settingsBtn;
     settingsBtn.setShadow(0, 4, '#0B0500', 4);
     settingsBtn.setInteractive();
 
     const statisticsBtn = this.add.text(96, 464, 'Statistics', menuItemsStyle);
+    this.statisticsBtn = statisticsBtn;
     statisticsBtn.setShadow(0, 4, '#0B0500', 4);
     statisticsBtn.setInteractive();
 
     const creditsBtn = this.add.text(96, 524, 'Credits', menuItemsStyle);
+    this.creditsBtn = creditsBtn;
     creditsBtn.setShadow(0, 4, '#0B0500', 4);
     creditsBtn.setInteractive();
-
-    // resumeBtn.setInteractive();
-    // this.resumeBtn.inputEnabled;
-    // this.resumeBtn.useHandCursor = true;
-
-    // const resumeBtn = this.add.text(95, 110, 'Resume', {
-    //   fontFamily: '"Oswald"',
-    //   fontSize: '36px',
-    // });
-
-    // this.resumeBtn = this.add
-    //   .text(
-    //     this.cameras.main.centerX,
-    //     this.cameras.main.centerY - 150,
-    //     'RESUME GAME',
-    //     { font: 'bold 46px Arial', fill: '#FAFAD2', cursor: 'pointer' }
-    //   )
-    //   .setOrigin(0.5)
-    //
-    // // eslint-disable-next-line no-unused-expressions
-    // this.resumeBtn.inputEnabled;
-    // this.resumeBtn.useHandCursor = true;
-
-    // this.onePlayerBtn = this.add
-    //   .text(
-    //     this.cameras.main.centerX,
-    //     this.cameras.main.centerY - 100,
-    //     'ONE PLAYER',
-    //     { font: 'bold 46px Arial', fill: '#FAFAD2', cursor: 'pointer' }
-    //   )
-    //   .setOrigin(0.5)
-    //   .setInteractive();
-
-    // this.twoPlayerBtn = this.add
-    //   .text(
-    //     this.cameras.main.centerX,
-    //     this.cameras.main.centerY - 50,
-    //     'TWO PLAYER',
-    //     { font: 'bold 46px Arial', fill: '#FAFAD2', cursor: 'pointer' }
-    //   )
-    //   .setOrigin(0.5)
-    //   .setInteractive();
-
-    // this.settingsBtn = this.add
-    //   .text(this.cameras.main.centerX, this.cameras.main.centerY, 'SETTINGS', {
-    //     font: 'bold 46px Arial',
-    //     fill: '#FAFAD2',
-    //     cursor: 'pointer',
-    //   })
-    //   .setOrigin(0.5)
-    //   .setInteractive();
-
-    // this.statisticsBtn = this.add
-    //   .text(
-    //     this.cameras.main.centerX,
-    //     this.cameras.main.centerY + 50,
-    //     'STATISTICS',
-    //     { font: 'bold 46px Arial', fill: '#FAFAD2', cursor: 'pointer' }
-    //   )
-    //   .setOrigin(0.5)
-    //   .setInteractive();
   }
 
   setEvents() {
-    this.resumeBtn.on(
-      'pointerdown',
-      function (event) {
-        this.scene.resume('Game');
-      },
-      this
-    );
     this.onePlayerBtn.on('pointerdown', this.selectMap, this);
     this.twoPlayerBtn.on('pointerdown', this.requestGame, this);
     this.settingsBtn.on('pointerdown', this.selectSettings, this);
